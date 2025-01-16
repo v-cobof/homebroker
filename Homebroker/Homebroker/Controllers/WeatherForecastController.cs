7a6f5689d2a0fbdcf98590368593e2f88dedd551
Victor Cobo <v.cobof@gmail.com>
2025-01-16 19:04:12 -0300

    Removed unused classes from Market project. Created homebroker API project

--- Homebroker/Homebroker/Controllers/WeatherForecastController.cs
+++ Homebroker/Homebroker/Controllers/WeatherForecastController.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 
-namespace Market.Controllers
+namespace Homebroker.Controllers
 {
     [ApiController]
     [Route("[controller]")]
--- Homebroker/Homebroker/WeatherForecast.cs
+++ Homebroker/Homebroker/WeatherForecast.cs
@@ -1,4 +1,4 @@
-namespace Market
+namespace Homebroker
 {
     public class WeatherForecast
     {
