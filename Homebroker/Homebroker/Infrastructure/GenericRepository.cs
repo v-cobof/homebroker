e12d5996cf0aa5f900e7904bc48085d8707fb0b5
Victor Cobo <v.cobof@gmail.com>
2025-01-22 00:03:45 -0300

    created data access

---
+++ Homebroker/Homebroker/Infrastructure/GenericRepository.cs
@@ -0,0 +1,46 @@
+﻿using Homebroker.Domain.Interfaces;
+using MongoDB.Driver;
+
+namespace Homebroker.Infrastructure
+{
+    public class GenericRepository<T> : IRepository<T> where T : class
+    {
+        protected readonly IMongoCollection<T> _collection;
+
+        public GenericRepository(IMongoClient mongoClient)
+        {
+            var database = mongoClient.GetDatabase("mongodb");
+            _collection = database.GetCollection<T>(typeof(T).Name);
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            return await _collection.Find(_ => true).ToListAsync();
+        }
+
+        public async Task<T> GetByIdAsync(string id)
+        {
+            return await _collection.Find(Builders<T>.Filter.Eq("_id", id)).FirstOrDefaultAsync();
+        }
+
+        public async Task CreateAsync(T entity)
+        {
+            await _collection.InsertOneAsync(entity);
+        }
+
+        public async Task UpdateAsync(string id, T entity)
+        {
+            await _collection.ReplaceOneAsync(Builders<T>.Filter.Eq("_id", id), entity);
+        }
+
+        public async Task DeleteAsync(string id)
+        {
+            await _collection.DeleteOneAsync(Builders<T>.Filter.Eq("_id", id));
+        }
+
+        public IQueryable<T> GetQueryable()
+        {
+            return _collection.AsQueryable();
+        }
+    }
+}
---
+++ Homebroker/Homebroker/Infrastructure/WalletAssetRepository.cs
@@ -0,0 +1,55 @@
+﻿using Homebroker.Application.DTO;
+using Homebroker.Domain;
+using Homebroker.Domain.Interfaces;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace Homebroker.Infrastructure
+{
+    public class WalletAssetRepository : GenericRepository<WalletAsset>, IWalletAssetRepository
+    {
+        public WalletAssetRepository(IMongoClient mongoClient) : base(mongoClient)
+        {
+        }
+
+        public async Task<IEnumerable<WalletAssetOutputDTO>> GetWalletAssetsByWalletId(string walletId)
+        {
+            var pipeline = new[]
+            {
+                new BsonDocument
+                {
+                    { "$lookup", new BsonDocument
+                        {
+                            { "from", "Asset" },         // Coleção com os detalhes dos ativos
+                            { "localField", "AssetId" },  // Campo na coleção walletAssets
+                            { "foreignField", "_id" },    // Campo correspondente na coleção assets
+                            { "as", "Asset" }            // Nome do campo no resultado
+                        }
+                    }
+                },
+                new BsonDocument
+                {
+                    { "$unwind", "$Asset" }  // Garante que 'Asset' será um objeto único, não uma lista
+                },
+                new BsonDocument
+                {
+                    { "$project", new BsonDocument
+                        {
+                            { "_id", 0 },
+                            { "AssetId", 1 },
+                            { "WalletId", 1 },
+                            { "Shares", 1 },
+                            { "Asset", 1 }  // Inclui os assets como lista
+                        }
+                    }
+                }
+            };
+
+            var result = await _collection
+                .Aggregate<WalletAssetOutputDTO>(pipeline)
+                .ToListAsync();
+
+            return result;
+        }
+    }
+}
